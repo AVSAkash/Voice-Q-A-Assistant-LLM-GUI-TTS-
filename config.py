3e7effc7dd61e1028495ef4b69d101816287ded8
AVSAkash <148715408+AVSAkash@users.noreply.github.com>
2025-07-20 20:29:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-OPENROUTER_API_KEY = "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"
+OPENROUTER_API_KEY = "Your_api_key_here"
 MODEL = "mistralai/mistral-7b-instruct:free"
